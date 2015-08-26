e5772f4266eb3ee130e721e597a7a16255afb705
ranjingfu <ranjingfu@zhouxiaojiedeiMac.local>
2015-08-26 16:14:37 +0800

    Initial Commit

---
+++ diceApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  diceApp
+//
+//  Created by ranjingfu on 15/8/26.
+//  Copyright (c) 2015年 ranjingfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ diceApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  diceApp
+//
+//  Created by ranjingfu on 15/8/26.
+//  Copyright (c) 2015年 ranjingfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
